e97832393a8a484887998e3a54e3ea4a948dc6f1
SDE00 <ongdongeun00@gmail.com>
2025-05-01 13:58:10 +0900

    간단한 설명 추가 (의미 x)

--- app/main.py
+++ app/main.py
@@ -6,6 +6,6 @@ from app.api import stt_feedback, quiz_generator, interview_router
 app = FastAPI()
 
 # TODO: 라우터 등록 - 기능별로 라우터 추가해서 변경해주세요
-app.include_router(quiz_generator.router)
-app.include_router(interview_router.router)
+#app.include_router(quiz_generator.router)
+#app.include_router(interview_router.router)
 app.include_router(stt_feedback.router, prefix="/stt")
