07e45526970fe7d5108dbc4ad245d7b653ac7420
tykong08 <141493823+tykong08@users.noreply.github.com>
2025-05-07 19:03:09 +0900

    main.py 업데이트

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from app.api import router  # __init__.py에서 통합된 router
+from app.api.question_generator.question_generator_model import initialize_llm
 
 app = FastAPI()
 app.include_router(router)
+
+@app.on_event("startup")
+async def startup_event():
+    initialize_llm()
