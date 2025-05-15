d27f98c52fc7b6379c7f9ad6d9388acaa01532ce
Chan-980730 <dubblem@naver.com>
2025-05-15 13:35:37 +0900

    batch size 수정

--- vector_db/chroma_client.py
+++ vector_db/chroma_client.py
@@ -8,7 +8,7 @@ collection = chroma_client.get_or_create_collection(COLLECTION_NAME)
 def generate_id(text: str) -> str:
     return hashlib.md5(text.encode('utf-8')).hexdigest()
 
-def save_to_vectorstore(questions, embeddings, batch_size=5000):
+def save_to_vectorstore(questions, embeddings, batch_size=1024):
     ids = [generate_id(q) for q in questions]
 
     for i in range(0, len(questions), batch_size):
