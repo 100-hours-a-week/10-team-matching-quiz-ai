7af2b4c178c63bf878a52fe046141df1ea8b553a
tykong08 <taeyoung.kong@outlook.com>
2025-05-19 15:17:53 +0900

    change folder

--- app/vector_db/retriever.py
+++ app/vector_db/retriever.py
@@ -7,9 +7,9 @@ import torch
 import logging
 import numpy as np
 import re
-from utils import embed_texts, clean_keyword_phrase, get_keyword_model
-from chroma_client import get_all_documents_with_vectors
-from config import RAG_TOP_K, SIM_THRESHOLD, RAG_DIVERSITY
+from vector_db.utils import embed_texts, clean_keyword_phrase, get_keyword_model
+from vector_db.chroma_client import get_all_documents_with_vectors
+from vector_db.config import RAG_TOP_K, SIM_THRESHOLD, RAG_DIVERSITY
 
 def extract_keywords_fallback(text: str, fallback_n: int = 3) -> List[str]:
     try:
