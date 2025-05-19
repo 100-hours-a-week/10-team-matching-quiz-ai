9e95e309631075489af92c6064452e02d94de202
tykong08 <taeyoung.kong@outlook.com>
2025-05-19 13:05:30 +0900

    change dir

--- vector_db/init_data.py
+++ vector_db/init_data.py
@@ -19,4 +19,4 @@ def init_vector_store_from_csv(csv_path: str):
     print(f"총 {len(questions)}개의 질문이 저장되었습니다.")
 
 if __name__ == "__main__":
-    init_vector_store_from_csv("./vector_db/question_data.csv")
+    init_vector_store_from_csv("vector_db/init_data.py")
