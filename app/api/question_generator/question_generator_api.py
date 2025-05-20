477a05cbb71fa8a9c235c31a454f8c408c07eac9
Chan-980730 <dubblem@naver.com>
2025-05-20 15:20:55 +0900

    Update question_generator_api.py

--- app/api/question_generator/question_generator_api.py
+++ app/api/question_generator/question_generator_api.py
@@ -11,6 +11,7 @@ from typing import List, Dict, Any, Optional
 from dotenv import load_dotenv
 import sys
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../")))
 # vector_db 모듈이 존재하는지 확인
 try:
     from vector_db.retriever import rag_retriever
