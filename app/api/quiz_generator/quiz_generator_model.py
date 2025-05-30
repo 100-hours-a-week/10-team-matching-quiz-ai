eaa7a9d93b2414441c9237ce02561b03efd6f097
Chan-980730 <dubblem@naver.com>
2025-05-30 10:43:42 +0900

    HF_TOKEN 추가

--- app/api/quiz_generator/quiz_generator_model.py
+++ app/api/quiz_generator/quiz_generator_model.py
@@ -1,8 +1,11 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 from app.api.quiz_generator.quiz_generator_config import QUIZ_MODEL_NAME
+from app.api.quiz_generator.quiz_generator_config import QUIZ_HF_TOKEN
+from huggingface_hub import login
 
-# 1. 디바이스 설정: Mac M1/M2는 mps, 그 외는 cpu
+
+# 디바이스 설정: Mac M1/M2는 mps, 그 외는 cpu
 if torch.cuda.is_available():
     device = "cuda"
 elif torch.backends.mps.is_available():
@@ -11,17 +14,18 @@ else:
     device = "cpu"
 print(f"디바이스 설정됨: {device}")
 
-# 2. 토크나이저 로딩
+# 토크나이저 로딩
+login(QUIZ_HF_TOKEN)
 tokenizer = AutoTokenizer.from_pretrained(QUIZ_MODEL_NAME, trust_remote_code=True)
 
-# 3. 모델 로딩 후 디바이스 지정
+# 모델 로딩 후 디바이스 지정
 model = AutoModelForCausalLM.from_pretrained(
     QUIZ_MODEL_NAME,
     torch_dtype=torch.float16 if device != "cpu" else torch.float32,
     trust_remote_code=True
-).to(device)  # 👉 모델을 직접 디바이스로 이동
+).to(device)  
 
-# 4. generate_quiz 함수 내에서도 입력을 같은 디바이스로 이동
+# generate_quiz 함수 내에서도 입력을 같은 디바이스로 이동
 def generate_quiz(prompt: str, max_tokens: int = 300) -> str:
     print("prompt 생성 및 디바이스 전송 중...")
     inputs = tokenizer(prompt, return_tensors="pt").to(device)  
