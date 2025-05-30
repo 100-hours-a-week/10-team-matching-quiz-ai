b65bbd63defc97e6af18a0f2dce93099ccbc6271
tykong08 <taeyoung.kong@outlook.com>
2025-05-30 10:32:44 +0900

    ✅ Update config.py)to set default values for EMBEDDING_MODEL_NAME and update collection names

--- app/vector_db/config.py
+++ app/vector_db/config.py
@@ -4,12 +4,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Model Config
-EMBEDDING_MODEL_NAME = os.getenv("EMBEDDING_MODEL_NAME")
+EMBEDDING_MODEL_NAME = os.getenv("EMBEDDING_MODEL_NAME", "Alibaba-NLP/gte-multilingual-base")
 KEYBERT_MODEL_NAME = os.getenv("KEYBERT_MODEL_NAME")
 
 # vector DB Config
 DB_PATH = os.getenv("DB_PATH", "./chroma_db")
-COLLECTION_NAME = os.getenv("COLLECTION_NAME", "questions")
+FOLLOW_UP_COLLECTION_NAME = os.getenv("FOLLOW_UP_COLLECTION_NAME", "questions")
+QUIZ_COLLECTION_NAME = os.getenv("QUIZ_COLLECTION_NAME", "quiz-generation")
 
 # RAG Parameters
 RAG_TOP_K = int(os.getenv("RAG_TOP_K", 4))
