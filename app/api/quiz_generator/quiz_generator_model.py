b6bdb3ef92b731274ce69cee24ef9e2ec0218938
Chan-980730 <dubblem@naver.com>
2025-06-02 09:18:54 +0900

    max_tokens 3000으로 변경

--- app/api/quiz_generator/quiz_generator_model.py
+++ app/api/quiz_generator/quiz_generator_model.py
@@ -26,7 +26,7 @@ model = AutoModelForCausalLM.from_pretrained(
 ).to(device)  
 
 
-def generate_quiz(prompt: str, max_tokens: int = 2000) -> str:
+def generate_quiz(prompt: str, max_tokens: int = 3000) -> str:
     print("prompt 생성 및 디바이스 전송 중...")
     inputs = tokenizer(prompt, return_tensors="pt").to(device)  
 
