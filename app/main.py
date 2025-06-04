960fe7f53434fcb7e7103e4cbc4cbcd6e31019e5
tykong08 <taeyoung.kong@outlook.com>
2025-06-04 10:14:46 +0900

    gcp 테스트 main

--- app/main.py
+++ app/main.py
@@ -221,3 +221,8 @@
 #     }
 
 
+from fastapi import FastAPI
+from app.api.quiz_generator.quiz_generator_api import router as quiz_router
+
+app = FastAPI()
+app.include_router(quiz_router, prefix="/quiz", tags=["quiz"])
