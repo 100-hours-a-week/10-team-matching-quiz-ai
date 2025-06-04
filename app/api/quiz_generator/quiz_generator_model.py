02d82009ca7ee80401799a1a0154ffb35ea02e57
Chan-980730 <dubblem@naver.com>
2025-06-05 05:21:50 +0900

    프롬프트 제거 로직

--- app/api/quiz_generator/quiz_generator_model.py
+++ app/api/quiz_generator/quiz_generator_model.py
@@ -34,17 +34,15 @@ model = AutoModelForCausalLM.from_pretrained(
 ).to(device)
 
 # 퀴즈 생성 함수
-def generate_quiz(prompt: str, max_tokens: int = 2000) -> str:
+def generate_quiz(prompt: str, max_tokens: int = 3000) -> str:
     print("prompt 생성 및 디바이스 전송 중...")
 
     prompt_tokens = tokenizer(prompt)['input_ids']
     print(f"[DEBUG] Prompt token 수: {len(prompt_tokens)}")
 
     # context window 제한
-    # max_context = 8196 - max_tokens
-    inputs = tokenizer(prompt, return_tensors="pt"
-                       #, truncation=True, max_length=max_context
-                       ).to(device)
+    max_context = 8196 - max_tokens
+    inputs = tokenizer(prompt, return_tensors="pt", truncation=True, max_length=max_context).to(device)
 
     print("quiz generate 시작")
     output = model.generate(
