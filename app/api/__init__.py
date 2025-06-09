45ebe094ea94c2881721cb090230d5d6e7e2defe
Chan-980730 <dubblem@naver.com>
2025-06-09 16:50:03 +0900

    __init.py 추가

---
+++ app/api/__init__.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+from app.api.question_generator.question_generator_api import router as generate_router
+from app.api.quiz_generator.quiz_generator_api import router as quiz_router
+
+router = APIRouter()
+router.include_router(generate_router, prefix="/interview", tags=["question-generator"])
+router.include_router(quiz_router, prefix="/quiz", tags=["quiz"])
