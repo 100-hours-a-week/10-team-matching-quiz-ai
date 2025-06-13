dbf4fa05278c9c8005dbc587ae3cb89d336f124c
tykong08 <taeyoung.kong@outlook.com>
2025-06-13 13:23:44 +0900

    chang host

--- app/config/rabbitmq_config.py
+++ app/config/rabbitmq_config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # RabbitMQ Server Details (AWS)
-RABBITMQ_HOST = "43.203.77.116"
+RABBITMQ_HOST = "mq.wingterview.com"
 RABBITMQ_PORT = 5672
 RABBITMQ_USER = os.getenv('RABBITMQ_USER')  
 RABBITMQ_PASSWORD = os.getenv('RABBITMQ_PASSWORD')  
-RABBITMQ_VIRTUAL_HOST = "/"
+RABBITMQ_VIRTUAL_HOST = "/" 
 
 # --- Quiz Generation Flow ---
 
