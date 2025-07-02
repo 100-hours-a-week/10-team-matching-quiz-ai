8852b64999d03439575e51687c4ceea714f3d292
Chan-980730 <dubblem@naver.com>
2025-07-02 16:01:44 +0900

    finetune 데이터 수정

--- app/api/quiz_generator/quiz_generator_finetune/train.py
+++ app/api/quiz_generator/quiz_generator_finetune/train.py
@@ -79,7 +79,7 @@ tokenizer.save_pretrained(f"{output_dir}/final_model")
 print("훈련 완료 및 모델 저장됨:", output_dir)
 
 def main():
-    trainer.train("./qwen3_lora_output_20250701_043758/checkpoint-1000")
+    trainer.train()
 
     # ─── 모델 저장 ─────────────────────────────────────────────
     model.save_pretrained(f"{output_dir}/final_model")
